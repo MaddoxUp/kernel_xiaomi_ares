2b099f62f3e008db4ae0172cc3a5ade6d56ae925
MaddoxUp <xiaoduoxiaoduo@hotmail.com>
2025-11-12 08:46:15 +0800

    add ksu

--- build.sh
+++ build.sh
@@ -5,20 +5,26 @@ function compile()
 
 source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
-ccache -M 100G
+ccache -M 50G
 export ARCH=arm64
-export KBUILD_BUILD_HOST=MARKxDEVS
-export KBUILD_BUILD_USER="AbzRaider"
+export KBUILD_BUILD_HOST=WSLUbuntu
+export KBUILD_BUILD_USER="Maddox"
+ if ! [ -d "clang" ]; then
 git clone --depth=1  https://gitlab.com/LeCmnGend/proton-clang.git -b clang-13  clang
-
-
+ fi
+echo "try setup kernelSU"
+ if ! [ -d "kernelSU" ]; then
+curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
+ fi
 
  if ! [ -d "out" ]; then
 echo "Kernel OUT Directory Not Found . Making Again"
 mkdir out
-fi
+
 
 make O=out ARCH=arm64 ares_user_defconfig
+
+ fi
 
 PATH="${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}" \
 make -j$(nproc --all) O=out \
@@ -37,14 +43,5 @@ make -j$(nproc --all) O=out \
                       CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log 
 }
 
-function zupload()
-{
-rm -rf AnyKernel	
-git clone --depth=1 https://github.com/AbzRaider/AnyKernel33 -b ares AnyKernel
-cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
-cd AnyKernel
-zip -r9 Test-OSS-KERNEL-ARES-S.zip *
-curl --upload-file "Test-OSS-KERNEL-ARES-S.zip" https://free.keep.sh
-}
 compile
-zupload
+
